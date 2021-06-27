8d852520fb1dab111f37198d4e02757d863624b1
Sandipan0110 <sandipanseal11@gmail.com>
2021-06-27 13:02:17 +0530

    number of times

--- bridgelabz/terminalcommands/problemStatment/flipCoinSimulation/flipCoinSimulation.sh
+++ bridgelabz/terminalcommands/problemStatment/flipCoinSimulation/flipCoinSimulation.sh
@@ -1,16 +1,29 @@
-#!/bin/bash -x
-
+declear -A flipValue
+isFlip=0;
 isHead=0;
 isTail=1;
-flipCheck=$((RANDOM%2))
+H=0;
+T=0;
+#read -p "Enter how many times you want to flip the coin : " flipCoinTimes
+flipCoinTimes=42;
+while  [[ isFlip -lt flipCoinTimes ]]
+do
+	flipCoin=$(( RANDOM%2 ))
+	case $flipCoin in
+        	$isHead)
+                	result=Heads
+			((H++))
+                	;;
+        	$isTail)
+                	result=Tails
+			((T++))
+                	;;
+	esac
+	flipValue[$result]=$flipCoin
+	echo $result
+	isFlip=$(( $isFlip + 1 ))
+done
 
-
-case $flipCheck in
-	$isHead)
-		result=Heads
-		;;
-	$isTail)
-		result=Tails
-		;;
-esac
-echo $result "is the winner"
+echo "${flipValue[@]}"
+echo "Heads win $H Times"
+echo "Tails win $T Times"
